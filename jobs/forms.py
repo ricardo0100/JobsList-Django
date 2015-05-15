56b81b257a688cc7a4481b13f9211b3085735ac1
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2015-05-15 08:51:34 -0300

    Validação form login

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,13 +1,23 @@
 from django import forms
+from jobs.template_snippets import CustomFormErrorList
 
 
 class LoginForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.error_class = CustomFormErrorList
 
     email = forms.EmailField(label='E-mail', error_messages={'required': 'Informe seu e-mail'})
     senha = forms.CharField(widget=forms.PasswordInput, error_messages={'required': 'Informe sua senha'})
 
 
 class CadastroUsuarioForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        super(CadastroUsuarioForm, self).__init__(*args, **kwargs)
+        self.error_class = CustomFormErrorList
+
     nome = forms.CharField(label='Nome', error_messages={'required': 'Informe seu nome'})
     email = forms.EmailField(label='E-mail', error_messages={'required': 'Informe seu e-mail'})
     senha = forms.CharField(widget=forms.PasswordInput, error_messages={'required': 'Informe sua senha'})
--- jobs/views.py
+++ jobs/views.py
@@ -2,11 +2,10 @@ import django
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.template import RequestContext, loader
 from jobs.forms import LoginForm, CadastroUsuarioForm
-from jobs.template_snippets import CustomFormErrorList
 
 
 @login_required(login_url='/login')
@@ -18,47 +17,58 @@ def index(request):
     return HttpResponse(template.render(context))
 
 
-def login(request):
+def login(request, mostrar_mensagem_logout=False):
     if request.method == 'POST':
-        form = LoginForm(request.POST, error_class=CustomFormErrorList)
+        form = LoginForm(request.POST)
         if form.is_valid():
-            user = User.objects.filter(email=form.cleaned_data['email'])
-            if not user:
-                raise Http404
-            username = user[0].username
-            password = form.cleaned_data['senha']
+            email = form.cleaned_data['email']
+            if User.objects.filter(email=email).exists():
+                user = User.objects.filter(email=email)
+                username = user[0].username
+                password = form.cleaned_data['senha']
 
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    django.contrib.auth.login(request, user)
-                    return redirect("/")
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    if user.is_active:
+                        django.contrib.auth.login(request, user)
+                        return redirect("/")
+                    else:
+                        form.add_error(None, 'Usuário inativo')
                 else:
-                    raise Http404
+                    form.add_error(None, 'Senha incorreta')
             else:
-                raise Http404
+                form.add_error(None, 'Usuário não existe')
     else:
-        form = LoginForm(error_class=CustomFormErrorList)
+        form = LoginForm()
 
     template = loader.get_template('autenticacao/login.html')
     context = RequestContext(request, {
-        'form': form
+        'form': form,
+        'mostrar_mensagem_logout': mostrar_mensagem_logout
     })
     return HttpResponse(template.render(context))
 
 
 def logout(request):
     django.contrib.auth.logout(request)
-    template = loader.get_template('autenticacao/logout.html')
-    context = RequestContext(request, {
-
-    })
-    return HttpResponse(template.render(context))
+    return login(request, mostrar_mensagem_logout=True)
 
 
 def cadastro(request):
     if request.method == 'POST':
         form = CadastroUsuarioForm(request.POST)
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            senha = form.cleaned_data['senha']
+
+            confirmacao_senha = form.cleaned_data['confirmacao_senha']
+
+            user = User.objects.create_user(nome, email, senha)
+            user.save()
+
+            return login(request)
+
     else:
         form = CadastroUsuarioForm()
 
