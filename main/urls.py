004549ef4f09de2bd0e1f5439b5f979c3d5433a0
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2015-05-19 00:00:08 -0300

    mais bugs

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('jobs.urls')),
-    # url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
